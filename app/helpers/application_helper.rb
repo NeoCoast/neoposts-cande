957c2ab7df63bdda2f01d741b80489047e1ef4c8
Candelaria Lopez Castilla <candelarialopezcastilla@Candelarias-MacBook-Pro.local>
2023-10-10 09:24:27 -0300

    fix comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
 
-# app/helpers/application_helper.rb
-
 module ApplicationHelper
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-# app/mailers/application_mailer.rb
-
-# ApplicationMailer is responsible for sending email notifications.
-
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
